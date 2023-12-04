d5f3742f60dd847e35944fb0561ca1fd498ce45d
gxnho <gxnho@naver.com>
2023-12-04 16:23:38 +0900

    2023.12.04 javaStart

---
+++ src/variable/Var2.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int a; //변수 선언
+        a = 10; //변수 초기화
+        int b = 20; //변수 초기화
+        //어딘가에 값을 보관해두고 필요할 때 값을 꺼내서 읽을 수 있는 저장소가 필요하다.
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
---
+++ src/variable/Var4.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        //변수는 다음과 같이 하나씩 선언할 수도 있고,
+
+        int c, d;
+        //다음과 같이 한번에 여러 변수를 선언할 수도 있다.
+    }
+}
