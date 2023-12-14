500811f5e259ba2e483268ce0c349b9198f4da33
gxnho <gxnho@naver.com>
2023-12-14 15:55:38 +0900

    12.14 Array

--- src/array/ArrayDi0.java
+++ src/array/ArrayDi0.java
@@ -13,16 +13,15 @@ public class ArrayDi0 {
         arr[1][2] = 6; //1행, 3열
 
         //0행 출력
-
-        System.out.print(arr[0][0]+"");//0열 출력
-        System.out.print(arr[0][1]+"");//1열 출력
-        System.out.print(arr[0][2]+"");//2열 출력
+        System.out.print(arr[0][0]+" ");//0열 출력
+        System.out.print(arr[0][1]+" ");//1열 출력
+        System.out.print(arr[0][2]+" ");//2열 출력
         System.out.println();//한 행이 끝나면 라인을 변경한다.
 
         //1행 출력
-        System.out.print(arr[1][0]+"");//0열 출력
-        System.out.print(arr[1][1]+"");//1열 출력
-        System.out.print(arr[1][2]+"");//2열 출력
+        System.out.print(arr[1][0]+" ");//0열 출력
+        System.out.print(arr[1][1]+" ");//1열 출력
+        System.out.print(arr[1][2]+" ");//2열 출력
         System.out.println();//한 행이 끝나면 라인을 변경한다.
     }
 }
---
+++ src/array/ex/ArrayEx6.java
@@ -0,0 +1,7 @@
+package array.ex;
+
+public class ArrayEx6 {
+    public static void main(String[] args) {
+
+    }
+}
